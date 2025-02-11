2c9e5ee9aeeb0318ab43bf80e9b09b4d59211519
Achu-kappithan <achukappithan4236@gmail.com>
2025-02-11 18:13:11 +0530

    Implement shortest  path and cycleic detaction

--- GRAPH.js
+++ GRAPH.js
@@ -76,7 +76,9 @@ class graph {
             console.log(curr)
             visited.add(curr)
             for(let neigh of this.root.get(curr)){
-                que.push(neigh)
+                if(!visited.has(neigh)){
+                    que.push(neigh)
+                }
             }
         }
     }
@@ -89,14 +91,93 @@ class graph {
         }
     }
 
-    
+     //=====================================================    CLONE GRAPH    =======================================================
+
+
+    clonegraph(){
+        let clone = new graph()
+
+        for(let key of this.root.keys()){
+            clone.addVertex(key)
+        }
+
+        for (let [key,values] of this.root.entries()){
+            for(let val of values){
+                clone.addEdge(key,val)
+            }
+        }
+        return clone
+    }
+
+
+    //=====================================================   CYCLEIC DETACTION   ===================================================
+
+    cyclicDetaction(){
+        let visited = new Set()
+        let recstack = new Set()
+
+        let dfs = (root)=>{
+            if(recstack.has(root)) return true
+            if(visited.has(root)) return false
+
+            recstack.add(root)
+            visited.add (root)
+
+            for(let neigh of this.root.get(root)){
+                if(dfs(neigh)) return true
+            }
+            recstack.delete(root)
+            return false
+
+        }
+
+        for(let neigh of this.root.keys()){
+            if(!visited.has(neigh)){
+                if(dfs(neigh)) return true
+            }
+        }
+
+        return false
+    }
+
+    //=====================================================   SHORTEST PATH   ===================================================
+
+
+    shortestpat(start,end){
+        if(!this.root.has(start)  ||  !this.root.has(end)) return  -1
+
+        let que = [[start,0]]
+        let visited = new Set()
+        visited.add (start)
+
+        while(que.length){
+            let [curr,distance]  =que.shift()
+
+            if(curr == end ){
+                return distance
+            }
+
+            for(let neigh of this.root.get(curr)){
+                if(!visited.has(neigh)){
+                    que.push([neigh,distance+1])
+                }
+            }
+        }
+        return -1
+
+    }
+
 }
 
 let mygraph = new graph()
 mygraph.addVertex(50)
 mygraph.addVertex(70)
-mygraph.addEdge(70,50)
-mygraph.addEdge(30,60)
+mygraph.addEdge(70,60)
+mygraph.addEdge(50,70)
+mygraph.addEdge(60,30)
 mygraph.dfsall()
 console.log(mygraph)
 mygraph.bfsall()
+console.log(mygraph.clonegraph())
+console.log(mygraph.cyclicDetaction())
+console.log(mygraph.shortestpat(50,30))
